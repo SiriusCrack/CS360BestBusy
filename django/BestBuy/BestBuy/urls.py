b359a0cc4355035d37d0a79a09bfca646122bddf
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-04-18 16:45:05 -0700

    random html changes

--- django/BestBuy/BestBuy/urls.py
+++ django/BestBuy/BestBuy/urls.py
@@ -23,5 +23,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
-    path('', RedirectView.as_view(url='store/')),
+    path('', RedirectView.as_view(url='store/'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- django/BestBuy/store/views.py
+++ django/BestBuy/store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Brand, ProductType, DisplayType, OS, Product
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_prods = Product.objects.all().count()
+
+    context = {
+        'num_prods': num_prods,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
