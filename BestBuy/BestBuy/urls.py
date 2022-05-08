6c3f326c8a04fa175491769e8cff30f6dbab7fbc
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-05-08 15:50:55 -0700

    added login page

--- BestBuy/BestBuy/urls.py
+++ BestBuy/BestBuy/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('store/', include('store.urls')),
     path('', RedirectView.as_view(url='store/'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
